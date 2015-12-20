dd18b75adb0be026766439549e8ab8e1625ea8c1
aleksandrmaksimoviclt <aleksandr.maksimoviclt@gmail.com>
2015-12-20 17:32:30 +0200

    Changed bracnch to landing page development

--- landing/views.py
+++ landing/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Info, TeamMember
 
 def index(request):
-    return HttpResponse("Hello landing")
+    members = TeamMember.objects.all()
+    info = Info.objects.all()
+    return render(request, 'landing/index.html', {'members': members, 'info': info})
