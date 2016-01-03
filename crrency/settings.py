4de5538588df45d6f98fb1ff7ab79572c8720ebf
Dambre <lukas.dambr@gmail.com>
2016-01-03 13:20:47 +0200

    landing index added, settings: static dir added

--- crrency/settings.py
+++ crrency/settings.py
@@ -128,7 +128,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 ADMIN_MEDIA_PREFIX = '/static/admin/'
 
