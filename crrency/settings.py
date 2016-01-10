1cc6407db06ba3c655537f513d5a77fadd4f241e
Dambre <lukas.dambr@gmail.com>
2016-01-10 13:14:23 +0200

    database fix(maybe)

--- crrency/settings.py
+++ crrency/settings.py
@@ -94,11 +94,14 @@ import dj_database_url
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-DATABASES = {
+'''DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
     }
-}
+}'''
+DATABASES['default'] =  dj_database_url.config()
+
+
 
 
 # Password validation
