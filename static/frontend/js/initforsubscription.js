d861e8a8e44204ddfa35f8267cfc3e675b69ab3e
aleksandrmaksimoviclt <aleksandr.maksimoviclt@gmail.com>
2016-01-12 20:24:13 +0200

    Created form for emailsubmission

--- static/frontend/js/initforsubscription.js
+++ static/frontend/js/initforsubscription.js
@@ -1,4 +1,11 @@
 $(window).load(function(){
 	$("#preloader").addClass("hide");
 	$("#view").removeClass("hide");
+
+	$('#collect-email').on('submit', function(event){
+    event.preventDefault();
+    console.log("form submitted!")  // sanity check
+    create_post();
+});
+
 });
