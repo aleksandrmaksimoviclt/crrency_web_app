f0d60613d218f6ee1d8ad65cc84da8e79c2d8748
aleksandrmaksimoviclt <aleksandr.maksimoviclt@gmail.com>
2016-01-28 19:59:37 +0200

    Added feed layout

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 
+
 def index(request):
     return render(request, 'dashboard/index.html', {})
 
