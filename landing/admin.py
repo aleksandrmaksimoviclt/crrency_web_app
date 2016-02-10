b5a259138ccdba31b8adda6f07f1e36b477d1d96
Dambre <lukas.dambr@gmail.com>
2016-02-11 00:08:25 +0200

    admin.py remove unusable fields,

--- landing/admin.py
+++ landing/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from .models import Info, TeamMember, Subscriber
 # Register your models here.
-class InfoAdmin(admin.ModelAdmin):
-    list_display = ('name', 'date_time')
+#class InfoAdmin(admin.ModelAdmin):
+#   list_display = ('name', 'date_time')
 
-class TeamMemberAdmin(admin.ModelAdmin):
-    list_display = ('name', 'position', 'description')
-
-admin.site.register(Info, InfoAdmin)
-admin.site.register(TeamMember, TeamMemberAdmin)
+#class TeamMemberAdmin(admin.ModelAdmin):
+#    list_display = ('name', 'position', 'description')
+#
+#admin.site.register(Info, InfoAdmin)
+#admin.site.register(TeamMember, TeamMemberAdmin)
 admin.site.register(Subscriber)
